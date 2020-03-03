e1f9123088ee7aa2e0b4aeacd0a3f71d9536d975
= <roliveirasv@gmail.com>
2020-03-03 18:56:00 -0300

    Configuration of body-parser that is responsible to translate datas wrote from form to structure Javascript

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
 
 app.set('view engine','ejs');
-
-app.use(express.static('public'))
 var path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static('public'))
+
+//Body Parser
+//the code below is responsible  to translate the form's data into a javascript structure
+//extended:false
+app.use(bodyParser.urlencoded({extended:false}));
+
+//Allowing to read form's data sent from JSON. It's a lot of  used when we work with API.
+app.use(bodyParser.json());
+
+
+//Routers
 app.get('/',(req,res)=>{
     res.render('index');
 });
@@ -16,7 +27,9 @@ app.get('/question',(req,res)=>{
 });
 
 app.post('/savequestion',(req,res)=>{
-    res.render('savequestion');
+    var title = req.body.fieldtitle             // Body Parser is responsible to available the object body
+    var description = req.body.fielddescription // Body Parser is responsible to available the object body
+    res.render('savequestion',{title:title, description:description});
 })
 
 app.listen(3000,()=>{
