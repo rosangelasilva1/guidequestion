dcd2a2af3ddf84b46018e4809796197ab6ff2f16
= <roliveirasv@gmail.com>
2020-03-03 12:30:06 -0300

    first commit- building main form

--- index.js
+++ index.js
@@ -3,28 +3,19 @@ const app = express();
 
 app.set('view engine','ejs');
 
+app.use(express.static('public'))
+
 app.get('/',(req,res)=>{
     res.render('index');
 
 });
 
-app.get('/home/:nome/:sobrenome',(req,res)=>{
+app.get('/question',(req,res)=>{
     
-app.use(express.static('public'))
-
-    var nome = req.params.nome;
-    var sobrenome = req.params.sobrenome;
-    var exibemsg = false;
-    var categorias = [
-        {nome:"Cozinha",descricao:"Artigos para cozinha"},
-        {nome:"Escritório",descricao:"produtos para escritório"},
-        {nome:"Personalizados",descricao:"produtos feitos sob encomenda"}
-    ]
-    
-    res.render('home',{nome:nome,sobrenome:sobrenome,msg:exibemsg,categ:categorias});
+    res.render('question');
 
 });
 
 app.listen(3000,()=>{
-    console.log("app rodando");
+    console.log("app running");
 });
